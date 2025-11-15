8bc8631bb3037d71b986736f2742f2e1bd0e4228
proses-p <prosesprojestus0@gmail.com>
2025-11-15 17:48:34 +0300

    update for Render deployment

--- openclass/settings.py
+++ openclass/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8&#_v7a(vs7*ht0(30a-es^sg1ufes+b3##&(-frbtt5&eih)b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['openclass.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
