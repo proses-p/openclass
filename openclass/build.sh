2ee6f4941cf8ebccc78a53037d3348cdde771026
proses-p <prosesprojestus0@gmail.com>
2025-11-20 16:42:09 +0300

    Update build.sh for deployment

--- openclass/build.sh
+++ openclass/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+
 # exit on error
 set -o errexit
+
 pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
