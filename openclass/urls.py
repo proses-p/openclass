ba0ef3682b89acf4272de0c8b2623f4fb15173d7
proses-p <prosesprojestus0@gmail.com>
2026-06-26 12:45:23 +0300

    Add sitemap

--- openclass/urls.py
+++ openclass/urls.py
@@ -16,12 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from resources.sitemaps import StaticViewSitemap
 from django.conf import settings
 from django.conf.urls.static import static
 
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('resources.urls')),
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+
+name='django.contrib.sitemaps.views.sitemap'
+    ),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
